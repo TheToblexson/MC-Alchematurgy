9d5e49ce6e216b7ceb8e7a7987b7cbfa20f2b10f
Toblexson <71195995+TheToblexson@users.noreply.github.com>
2025-01-10 21:25:24 +0000

    Given the Alchemical Crucible a passable texture/model.

--- src/main/java/net/toblexson/alchematurgy/datagen/ModBlockStateProvider.java
+++ src/main/java/net/toblexson/alchematurgy/datagen/ModBlockStateProvider.java
@@ -1,6 +1,7 @@
 package net.toblexson.alchematurgy.datagen;
 
 import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
 import net.neoforged.neoforge.client.model.generators.BlockStateProvider;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import net.neoforged.neoforge.registries.DeferredBlock;
@@ -28,7 +29,10 @@ public class ModBlockStateProvider extends BlockStateProvider
     @Override
     protected void registerStatesAndModels()
     {
-        block(ModBlocks.ALCHEMICAL_CRUCIBLE);
+        simpleBlockWithItem(ModBlocks.ALCHEMICAL_CRUCIBLE.get(),
+            models().cubeBottomTop("alchemical_crucible", modBlockLocation("alchemical_crucible_side"),
+                                   modBlockLocation("alchemical_crucible_bottom"), modBlockLocation("alchemical_crucible_top")));
+
         block(ModBlocks.ALCHEMICAL_DISTILLER);
     }
 
@@ -39,5 +43,15 @@ public class ModBlockStateProvider extends BlockStateProvider
     private void block(DeferredBlock<?> deferredBlock)
     {
         simpleBlockWithItem(deferredBlock.get(), cubeAll(deferredBlock.get()));
+    }
+
+    /**
+     * Get a mod specific resource location
+     * @param path The desired path.
+     * @return The mod specific resource location.
+     */
+    private static ResourceLocation modBlockLocation(String path)
+    {
+        return ResourceLocation.fromNamespaceAndPath(Alchematurgy.MOD_ID, "block/"+path);
     }
 }
